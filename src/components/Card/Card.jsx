e86b360c2e8a522b6c7fb0938e93194c8c6dd46b
Cory Spicer <corys4139@gmail.com>
2021-01-03 19:28:12 -0500

    fix state setting on dating card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 class Card extends Component {
     state = { 
-        profile: {
-            picture: "",
-            name: "",
-            city: "",
-            age: ""
-        }
+        picture: "",
+        name: "",
+        city: "",
+        age: ""
      }
 
      handleYepClick = () => {
@@ -15,7 +13,7 @@ class Card extends Component {
         this.getProfile();
         this.props.handleYep();
         //  click.play()
-        console.log('yep')
+        console.log('yep', this.state)
      }
 
      handleNopeClick = () => {
@@ -24,7 +22,7 @@ class Card extends Component {
         this.getProfile();
         this.props.handleNope();
         // snap.play()
-        console.log('nope')
+        console.log('nope', this.state)
     }
 
      async getProfile(){
@@ -33,19 +31,17 @@ class Card extends Component {
             return response.json()
         })
         .then((data) => {
-            console.log(data)
             const name = data.results[0].name.first;
             const city = data.results[0].location.city;
             const age = data.results[0].dob.age;
             // profiles.push(newProfile);
             this.setState({
-                name: this.state.profile.name,
-                city: this.state.profile.city,
-                age: this.state.profile.age
-            }, () => {
-                console.log(this.state.profile)
+                name: name,
+                city: city,
+                age: age
+                }, () => {
+                console.log(this.state)
             })
-            console.log(name, city, age);
         })
         .catch((err) => {
             console.log(err)
@@ -58,28 +54,41 @@ class Card extends Component {
             return response.json()
         })
         .then((data) => {
-            console.log(data)
             const picture = data[parseInt(Math.floor(Math.random() * Math.floor(30)))].download_url;
             // profiles.push(newProfile);
             this.setState({
-                picture: this.state.profile.picture
+                picture: picture
             }, () => {
-                console.log(this.state.profile)
+                console.log(this.state)
             })
-            console.log(picture)
         })
         .catch((err) => {
             console.log(err)
         })
     }
+
+    componentDidMount(){
+        this.getProfilePic()
+        this.getProfile()
+    }
+
+    // testButton() {
+    //     const testState = this.state
+    //     console.log(testState)
+    // }
     
     render() { 
         return ( 
             <React.Fragment>
                 <h4>Dating Card goes here</h4>
+                <h4>{this.state.name}</h4>
+                <h4>{this.state.age}</h4>
+                <h4>{this.state.city}</h4>
+                <h4>{this.state.picture}</h4>
                 <div>
                     <button onClick={this.handleYepClick}>Yep</button>
                     <button onClick={this.handleNopeClick}>Nope</button>
+                    <button onClick={this.testButton}>Test</button>
                 </div>
             </React.Fragment>
          );
